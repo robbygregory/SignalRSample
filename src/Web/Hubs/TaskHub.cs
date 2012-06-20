ec65db4f3bea057440540624e21a450c45169775
Robby Gregory <robby.gregory@gmail.com>
2012-06-20 15:45:14 -0500

    initial checkin

---
+++ src/Web/Hubs/TaskHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using SignalR.Hubs;
+using System.Collections.Generic;
+
+namespace Web.Hubs
+{
+	public class TaskHub : Hub
+	{
+		private static readonly IList<string> _tasks = new List<string>();
+		
+		public string[]  Init()
+		{
+			return _tasks.ToArray();
+		}
+		
+		public void Add(string subject)
+		{
+			if (string.IsNullOrEmpty(subject))
+			{
+				throw new Exception("Subject cannot be null.");
+			}
+			_tasks.Add(subject);
+			Clients.notify(subject);
+		}
+	}
+}
